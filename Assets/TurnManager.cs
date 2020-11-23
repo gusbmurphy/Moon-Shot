bda21094fa11c1e9256e508ed323d0c52ca534f8
Gus <gusbmurphy@gmail.com>
2020-11-23 13:49:01 -0500

    Add turn counter

--- Assets/TurnManager.cs
+++ Assets/TurnManager.cs
@@ -1,10 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TurnManager : MonoBehaviour
 {
+    public Text turnText;
+
+    private int _turn;
+    private int Turn
+    {
+        get { return _turn; }
+        set
+        {
+            _turn = value;
+            turnText.text = _turn.ToString();
+        }
+    }
+
+    private void Start()
+    {
+        Turn = 1;
+    }
+
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
