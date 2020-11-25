02f7d760cd6c2ef04da431c0400726d0a833a5d3
Gus <gusbmurphy@gmail.com>
2020-11-25 11:44:10 -0500

    Add objective definition

---
+++ Assets/Scripts/ObjectiveDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveDefinition : MonoBehaviour
+{
+    public GameObject goal;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject == goal)
+        {
+            print("We've done it!");
+        }
+    }
+}
