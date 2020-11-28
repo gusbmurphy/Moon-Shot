ef55855899611c7b16191caf9ee477c9c707eb0a
Gus <gusbmurphy@gmail.com>
2020-11-28 18:16:34 -0500

    Make objective dissapear into goal

--- Assets/Scripts/ObjectiveDefinition.cs
+++ Assets/Scripts/ObjectiveDefinition.cs
@@ -14,14 +14,46 @@ public class ObjectiveDefinition : MonoBehaviour
         set
         {
             _isCompleted = value;
-            if (_isCompleted) completed.Invoke();
+            if (_isCompleted)
+            {
+                DisappearIntoGoal();
+                completed.Invoke();
+            }
         }
     }
 
     public UnityEvent completed;
 
+    private bool shouldLerpToGoal = false;
+    private Vector3 lerpStartPos;
+    private float t = 0.0f;
+    private float lerpTime = 1.5f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == goal) IsCompleted = true;
+    }
+
+    private void DisappearIntoGoal()
+    {
+        // Stop all current velocity
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3();
+
+        shouldLerpToGoal = true;
+        lerpStartPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (shouldLerpToGoal)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(
+                lerpStartPos,
+                goal.transform.position,
+                t / lerpTime);
+            if (t / lerpTime >= 1f) gameObject.SetActive(false);
+        }
     }
 }
