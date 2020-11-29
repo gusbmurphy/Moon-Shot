b21c2e9c2caa9be5f84c4539cc118d819f0b5ee5
Gus <gusbmurphy@gmail.com>
2020-11-29 13:01:19 -0500

    Add camera socket to cue prefab

---
+++ Assets/Scripts/Cue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue : MonoBehaviour
+{
+    public Transform cameraSocket;
+    public GameObject cue;
+}
