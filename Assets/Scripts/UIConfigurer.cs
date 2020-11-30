54a282ae74f04af42d2cb4280284f7d394d2131d
Gus <gusbmurphy@gmail.com>
2020-11-30 18:08:33 -0500

    Add UIConfigurer to correctly set canvas settings

---
+++ Assets/Scripts/UIConfigurer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIConfigurer : MonoBehaviour
+{
+    /* Currently, all this script is for is to ensure that the UI canvas's
+     * camera is correctly set. */
+    void Start()
+    {
+        Canvas canvas = GetComponentInChildren<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = Camera.main;
+    }
+}
