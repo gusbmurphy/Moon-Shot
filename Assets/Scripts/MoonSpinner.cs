c7dd2113105c340ee0685434afa29c187659841d
Gus <gusbmurphy@gmail.com>
2020-11-30 14:53:22 -0500

    Add MoonSpinner script

---
+++ Assets/Scripts/MoonSpinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonSpinner : MonoBehaviour
+{
+    public float degreesPerSecond = 3f;
+
+    void Update()
+    {
+        transform.Rotate(0, degreesPerSecond * Time.deltaTime, 0);
+    }
+}
