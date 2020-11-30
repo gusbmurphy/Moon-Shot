f4b71da6431f8ffda816e3979f2b564550c2d73d
Gus <gusbmurphy@gmail.com>
2020-11-30 14:53:34 -0500

    Implement title menu with start button

---
+++ Assets/Scripts/WorldSpaceButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WorldSpaceButton : MonoBehaviour
+{
+    public UnityEvent OnClick;
+
+    private Collider collider;
+    private bool isClicked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponentInChildren<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider == collider) isClicked = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider == collider)
+                {
+                    if (isClicked) OnClick.Invoke();
+                }
+                else if (isClicked)
+                {
+                    isClicked = false;
+                }
+            }
+        }
+    }
+}
